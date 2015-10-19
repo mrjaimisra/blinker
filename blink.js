8ec1e3a958d8deccbc7298486265ef5166b17a39
mrjaimisra <misrajai01@gmail.com>
2015-10-19 07:03:36 -0600

    Basic solution to first part of blink

--- blink.js
+++ blink.js
@@ -1,1 +1,27 @@
 // YOUR CODE GOES HERE
+//jQuery.noConflict();
+
+//.toggle( effect [, options ] [, duration ] [, complete ] )
+
+
+$.fn.blink = function (interval) {
+    this.hide(0).delay(interval).show(0).delay(interval).blink(interval);
+    return this;
+};
+
+//jQuery.fn.blink = function (interval) {
+//  setInterval(function () {
+//    this.css('visibility', 'hidden');
+//  }, interval);
+//  return this;
+//};
+
+// V1
+// show/hide every 1000ms
+// jQuery('.myDiv').blink(1000);
+// twice as fast
+// jQuery('.otherDiv').blink(500);
+
+// V2
+//jQuery('div').blink(1000);
+
